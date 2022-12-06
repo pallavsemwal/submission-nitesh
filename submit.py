48c58ba58571122cdf8b8b77707c4486baf9eef2
nitesh047 <pallav.semwal24@gmail.com>
2022-12-06 22:04:08 +0530

    sub

---
+++ submit.py
@@ -0,0 +1,23 @@
+
+import os
+dict={}
+# dict[book_name][0]-> for buy;
+# dict[book_name][1]-> for sell;
+def add_order(book_name, type, volume , price, id){
+    if(dict[book_name]){
+        if( type=='BUY'){
+            # apply the logic
+        }
+        else{
+            # apply the logic
+        }
+        # dict[book_name]
+    }
+}
+def create_book(name){
+    dict[name]={} 
+    # create a dictionary to maintain the orders
+}
+# # Check whether the specified
+# # path exists or not
+# print(isExist)
